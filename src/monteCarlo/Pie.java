8ee43b927bff2dd6780b1467396323393a20f5d9
Tom Seagrave <tomseagrave94@gmail.com>
2017-07-09 10:48:40 +0100

    First draft of monte carlo

---
+++ src/monteCarlo/Pie.java
@@ -0,0 +1,24 @@
+package monteCarlo;
+
+/**
+ * This class runs a monteCarlo integration to calculate pie. 
+ * @author thomasseagrave
+ *
+ */
+public class Pie {
+
+	/**
+	 * Calculates pie, using i iterations
+	 * @param i
+	 * @return 
+	 * @return 
+	 */
+	public int calculatePie(int i) {
+		int pie = 0;
+		
+		
+		return pie;
+		
+	}
+
+}
---
+++ src/monteCarlo/monteCarlo.java
@@ -0,0 +1,11 @@
+package monteCarlo;
+
+public class monteCarlo {
+
+	public static void main(String[] args) {
+		Pie p = new Pie();
+		p.calculatePie(1000);
+
+	}
+
+}
